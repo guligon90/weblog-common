6f4ce281d86386c8d26595e4cdb292535f5be94f
Guilherme Lima Goncalves <guilherme.goncalves@pland.com.br>
2022-02-21 14:34:01 -0300

    feat(events): include new subjects

--- src/events/subjects.ts
+++ src/events/subjects.ts
@@ -1,4 +1,10 @@
 export enum Subjects {
   PostCreated = 'post:created',
   PostUpdated = 'post:updated',
+  PostApproved = 'post:approved',
+  PostRejected = 'post:rejected',
+  CommentCreated = 'comment:created',
+  CommentUpdated = 'comment:updated',
+  CommentApproved = 'comment:approved',
+  CommentRejected = 'comment:rejected',
 }
