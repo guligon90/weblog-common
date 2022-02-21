fab52be3aadfe75729054af9433af4c489e5402c
Guilherme Lima Goncalves <guilherme.goncalves@pland.com.br>
2022-02-21 15:29:11 -0300

    feat(middlewares): improve logging in error handling

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,14 +1,23 @@
 import { Request, Response, NextFunction } from 'express';
 import { CustomError } from '../errors/custom-error';
+import { Logger } from '../logger';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+  const logger = Logger(`${err.name} @ ${__filename}`);
+
   if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
-  console.error(err);
-  res.status(400).send({
-    errors: [{ message: 'Something went wrong' }],
+  logger.error(err);
+
+  res.status(500).send({
+    errors: [
+      {
+        error: err.name,
+        message: err.message || 'Something went wrong',
+      },
+    ],
   });
 };
